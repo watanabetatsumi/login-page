6b91640ac5cd9f54f276701efdb51a13b5b0ec2f
watanabetatsumi <watanabe.tatsumi@icloud.com>
2025-03-31 15:49:45 +0900

    修正

--- infra/modules/cicd/main.tf
+++ infra/modules/cicd/main.tf
@@ -7,7 +7,7 @@ resource "aws_codedeploy_app" "cicd_app" {
 resource "aws_codedeploy_deployment_group" "cicd_group" {
     app_name = aws_codedeploy_app.cicd_app.name
     deployment_group_name = "cicd_group_EC2s"
-    service_role_arn = var.iam_code_deploy_arn
+    service_role_arn = "arn:aws:iam::864981749778:role/CodeDeploy_Service_Role"
 
     autoscaling_groups = [var.ec2_auto_scaling_group_name]
 
@@ -24,15 +24,19 @@ resource "aws_codedeploy_deployment_group" "cicd_group" {
         terminate_blue_instances_on_deployment_success {
             action = "TERMINATE"
         }
-    }
-
-    ec2_tag_set {
-        ec2_tag_filter {
-        key   = "CodeDeploy"
-        type  = "KEY_AND_VALUE"
-        value = "Target"
+        # Auto Scaling Group の自動コピーを有効化
+        green_fleet_provisioning_option {
+            action = "COPY_AUTO_SCALING_GROUP"
         }
     }
+
+    # ec2_tag_set {
+    #     ec2_tag_filter {
+    #     key   = "CodeDeploy"
+    #     type  = "KEY_AND_VALUE"
+    #     value = "Target"
+    #     }
+    # }
 
     load_balancer_info {
         target_group_info {
